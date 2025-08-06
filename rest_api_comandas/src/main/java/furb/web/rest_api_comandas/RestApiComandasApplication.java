b12ddcbe553f42628ff365b7fb4ee1dddf9e8abb
Luan Guarnieri <luanlg14022002@gmail.com>
2025-08-06 00:31:27 -0300

    init projeto

---
+++ rest_api_comandas/src/main/java/furb/web/rest_api_comandas/RestApiComandasApplication.java
@@ -0,0 +1,13 @@
+package furb.web.rest_api_comandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiComandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiComandasApplication.class, args);
+	}
+
+}
---
+++ rest_api_comandas/src/test/java/furb/web/rest_api_comandas/RestApiComandasApplicationTests.java
@@ -0,0 +1,13 @@
+package furb.web.rest_api_comandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiComandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
